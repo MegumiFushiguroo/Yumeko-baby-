c51e6a776d74b004b86fa3cd92d15eee13d8b426
MegumiFushiguroo <147046731+MegumiFushiguroo@users.noreply.github.com>
2024-01-16 16:22:23 +0530

    Update userbot.py

--- VipX/core/userbot.py
+++ VipX/core/userbot.py
@@ -48,9 +48,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Panther_community")
-                await self.one.join_chat("Panther_community")
-                await self.one.join_chat("Panther_community")
+                await self.one.join_chat("Ahjin_sprt")
+                await self.one.join_chat("Ahjin_sprt")
+                await self.one.join_chat("Ahjin_sprt")
             except:
                 pass
             assistants.append(1)
@@ -80,9 +80,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("Panther_community")
-                await self.two.join_chat("Panther_community")
-                await self.two.join_chat("Panther_community")
+                await self.two.join_chat("Ahjin_sprt")
+                await self.two.join_chat("Ahjin_sprt")
+                await self.two.join_chat("Ahjin_sprt")
             except:
                 pass
             assistants.append(2)
@@ -112,9 +112,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("Panther_community")
-                await self.three.join_chat("Panther_community")
-                await self.three.join_chat("Panther_community")
+                await self.three.join_chat("Ahjin_sprt")
+                await self.three.join_chat("Ahjin_sprt")
+                await self.three.join_chat("Ahjin_sprt")
             except:
                 pass
             assistants.append(3)
@@ -144,9 +144,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("Panther_community")
-                await self.four.join_chat("Panther_community")
-                await self.four.join_chat("Panther_community")
+                await self.four.join_chat("Ahjin_sprt")
+                await self.four.join_chat("Ahjin_sprt")
+                await self.four.join_chat("Ahjin_sprt")
             except:
                 pass
             assistants.append(4)
@@ -176,9 +176,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("Panther_community")
-                await self.five.join_chat("Panther_community")
-                await self.five.join_chat("Panther_community")
+                await self.five.join_chat("Ahjin_sprt")
+                await self.five.join_chat("Ahjin_sprt")
+                await self.five.join_chat("Ahjin_sprt")
             except:
                 pass
             assistants.append(5)
